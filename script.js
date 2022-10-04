c777eff5fbc6e6bc2f1ab0f11e7e54cdf2730183
Seynabou Sow <ssow2000@yahoo.com>
2022-10-03 22:09:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
